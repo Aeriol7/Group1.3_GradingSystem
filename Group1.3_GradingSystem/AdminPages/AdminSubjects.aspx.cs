35b701f8dc36cd2a8dc8d87549b7139433878c5a
Aeriol7 <aerioltrazona7@gmail.com>
2023-10-17 22:31:22 +0800

    AdminSubject

---
+++ Group1.3_GradingSystem/AdminPages/AdminSubjects.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Group1._3_GradingSystem.AdminPages
+{
+    public partial class AdminSubjects : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
