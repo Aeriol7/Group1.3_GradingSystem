1f75fdc283f4ba082dc27bfcff2106de9f7ef071
Rafael Rayco <rayco.rafaeljoseph.05032003@gmail.com>
2023-10-29 04:09:55 +0800

    Add teacher and student pages

---
+++ Group1.3_GradingSystem/StudentPages/StudentAnnouncementsPage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Group1._3_GradingSystem.StudentPages
+{
+	public partial class StudentAnnouncementsPage : System.Web.UI.Page
+	{
+
+		protected void lblStuHome_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("StudentHomePage.aspx");
+		}
+	}
+}
---
+++ Group1.3_GradingSystem/TeacherPages/TeacherLoginPage.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Group1._3_GradingSystem.TeacherPages
+{
+	public partial class TeacherLoginPage : System.Web.UI.Page
+	{
+		protected void btnLogin_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("TeacherHomePage.aspx");
+		}
+	}
+}
