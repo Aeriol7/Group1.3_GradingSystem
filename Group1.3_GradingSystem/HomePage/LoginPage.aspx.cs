3221e00b283b726eca6dd1dfac4d1b0b628eabdc
Rafael Rayco <rayco.rafaeljoseph.05032003@gmail.com>
2023-11-06 11:07:52 +0800

    Add login function

--- Group1.3_GradingSystem/HomePage/LoginPage.aspx.cs
+++ Group1.3_GradingSystem/HomePage/LoginPage.aspx.cs
@@ -14,8 +14,8 @@ namespace Group1._3_GradingSystem.HomePage
 		protected void btnLogin_Click(object sender, EventArgs e)
 		{
 
-			SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True;User Instance=True");
-			SqlCommand cmd = new SqlCommand("select * from tbl_data where username=@username and word=@word", con);
+			SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-O5EH83O;Initial Catalog=HIS_GradingSystem;Integrated Security=False;User Id=sa;Password=1234;MultipleActiveResultSets=True");
+			SqlCommand cmd = new SqlCommand("SELECT * FROM users WHERE username=@username and password=@password", con);
 			cmd.Parameters.AddWithValue("@username", txtUser.Text);
 			cmd.Parameters.AddWithValue("@password", txtPass.Text);
 			SqlDataAdapter sda = new SqlDataAdapter(cmd);
@@ -24,6 +24,26 @@ namespace Group1._3_GradingSystem.HomePage
 			con.Open();
 			int i = cmd.ExecuteNonQuery();
 			con.Close();
+
+			if (dt.Rows.Count > 0)
+			{
+				if (dt.Rows[0][3].ToString() == "Admin")
+				{
+					Response.Redirect("~/AdminPages/AdminHome.aspx");
+				}
+				else if (dt.Rows[0][3].ToString() == "Teacher")
+				{
+					Response.Redirect("~/TeacherPages/TeacherHomePage.aspx");
+				}
+				else if (dt.Rows[0][3].ToString() == "Student")
+				{
+					Response.Redirect("~/StudentPages/StudentHomePage.aspx");
+				}
+			}
+			else
+			{
+
+			}
 
 			//Response.Redirect("TeacherHomePage.aspx");
 		}
